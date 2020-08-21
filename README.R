c766f9ede93621444f6d0023a39fd30cfd0ece0e
Rodrupp <rodrupp@gmail.com>
2020-08-21 14:26:59 +1200

    added a line to test pushing the changes to github.

--- README.R
+++ README.R
@@ -1,2 +1,4 @@
 print("This is to emulate a time where I created a project locally and haven't created a repository for it, yet")
 print("I will now attempt to create a repository for it on github")
+
+print("this is to test pushing a change to github.")
